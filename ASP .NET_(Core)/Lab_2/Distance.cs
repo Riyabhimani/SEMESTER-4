e8c8111657a97913571776f227b0fd0bec658b66
Riyabhimani <146011638+Riyabhimani@users.noreply.github.com>
2024-12-19 19:16:48 +0530

    Add files via upload

---
+++ ASP .NET_(Core)/Lab_2/Distance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    //8. Write a program to Define a class Distance have data members dist1, dist2,
+    //dist3. Initialize the two data members using constructor and store their
+    //addition in third data member using function and display addition.
+    internal class Distance
+    {
+        public float dist1;
+        public float dist2;
+        public float dist3;
+
+        public Distance(float dist1, float dist2)
+        {
+            this.dist1 = dist1;
+            this.dist2 = dist2;
+        }
+
+        public void add()
+        {
+            dist3 = dist1 + dist2;
+            Console.WriteLine("Distance = " + dist3);
+        }
+    }
+}
---
+++ ASP .NET_(Core)/Lab_2/Salary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    //7. Write a program to Define a class Salary which will contain member variable Basic, TA, DA, HRA.
+    //Write a program using Constructor with default values for DA and HRA and calculate the salary of employee.
+    internal class Salary
+    {
+        public double Basic;
+        public double TA;
+        public double DA;
+        public double HRA;
+
+        public Salary(double basic, double tA, double d, double h)
+        {
+            Basic = basic;
+            TA = tA;
+            DA = d;
+            HRA = h;
+        }
+
+        public void salaryOfEmployee()
+        {
+            double sal = Basic + TA + DA + HRA;
+            Console.WriteLine("Salary of employee = " + sal);
+        }
+    }
+}
