4759b9f73173d06ad5c1823d87c8275af0f49c0b
Riyabhimani <146011638+Riyabhimani@users.noreply.github.com>
2025-06-13 20:00:14 +0530

    Add files via upload

---
+++ Python_Programming/LAB-11/Calculator.py
@@ -0,0 +1,13 @@
+def Addition(num1,num2):
+    return num1+num2
+
+def Subtraction(num1,num2):
+    return num1-num2
+
+def Multiplication(num1,num2):
+    return num1*num2
+
+def Division(num1,num2):
+    if num2 == 0:
+        return 'Can not divide by zero'
+    return num1/num2
